e0cbc6cdb845a0c3e952575c3b63df1ba9761eca
srx32 <kdavidmensah@gmail.com>
2024-04-21 11:57:46 +0000

    Installed cors (handle cors error/authorization) and morgan (for logging) packages

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
+import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(cors());
+
+// Logging middleware
+app.use(morgan("combined"));
 
 app.use(express.json());
 
