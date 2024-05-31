c113f55666b4d41cbe70fb2a467ad4e16181ea03
srx32 <kdavidmensah@gmail.com>
2024-05-31 23:46:12 +0000

    Installed mongoose package and established connection to database

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import * as http from "http";
 
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 import app from "./app";
 import bot from "./bot";
@@ -11,8 +12,24 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URL;
+
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`[server] - Listening on port ${PORT}...`);
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
 });
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function startServer() {
+  await mongoose.connect(MONGO_URL!);
+
+  server.listen(PORT, () => {
+    console.log(`[server] - Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
